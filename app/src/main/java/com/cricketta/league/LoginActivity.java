d41ec648c54f51d648761a04a033e25c555d305b
rahulxlash <rahulsharmpro@gmail.com>
2017-02-21 15:32:25 +0530

    Login Layout done, create user initial, create user grid and button with FB validate

--- app/src/main/java/com/cricketta/league/LoginActivity.java
+++ app/src/main/java/com/cricketta/league/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.cricketta.league;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.support.annotation.BoolRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -11,11 +13,14 @@ import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -26,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextView info;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+    Context mContext;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -35,41 +41,39 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mContext = this;
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
-        //info = (TextView)findViewById(R.id.info);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-//                info.setText(
-//                        "User ID: "
-//                                + loginResult.getAccessToken().getUserId()
-//                                + "\n" +
-//                                "Auth Token: "
-//                                + loginResult.getAccessToken().getUserId()
-//                );
-            }
+        if (Profile.getCurrentProfile().getId().isEmpty()) {
+            loginButton = (LoginButton) findViewById(R.id.login_button);
 
-            @Override
-            public void onCancel() {
+            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    Toast toast = Toast.makeText(mContext, loginResult.getAccessToken().getUserId(), Toast.LENGTH_SHORT);
+                    toast.show();
+                    Intent intent = new Intent(mContext, create_user_activity.class);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onCancel() {
 //                info.setText("Login attempt canceled.");
-            }
+                }
 
-            @Override
-            public void onError(FacebookException e) {
+                @Override
+                public void onError(FacebookException e) {
 //                info.setText("Login attempt failed.");
-            }
-        });
+                }
+            });
+        }else{
+            Intent intent = new Intent(mContext, create_user_activity.class);
+            startActivity(intent);
+        }
     }
-
-
 }
--- app/src/main/java/com/cricketta/league/create_user_activity.java
+++ app/src/main/java/com/cricketta/league/create_user_activity.java
@@ -1,7 +1,15 @@
 package com.cricketta.league;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class create_user_activity extends AppCompatActivity {
 
@@ -9,5 +17,42 @@ public class create_user_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user_activity);
+        GridView gridView = (GridView)findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id ){
+                Toast.makeText(create_user_activity.this, " " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+  public class ImageAdapter extends BaseAdapter {
+    private Context mcontext;
+      public ImageAdapter(Context c) {
+          mcontext = c;
+      }
+      public int getCount() {
+          return mThumbIds.length;
+      }
+      public Object getItem(int position) {
+          return null;
+      }
+      public long getItemId(int position) {
+          return 0;
+      }
+
+      public View getView(int position, View convertView, ViewGroup parent) {
+          ImageView imageView = new ImageView(mcontext);
+          imageView.setImageResource(mThumbIds[position]);
+          return imageView;
+      }
+     private Integer[] mThumbIds= {
+            R.drawable.prof_ico_1,
+            R.drawable.prof_ico_2,
+            R.drawable.prof_ico_3,
+            R.drawable.prof_ico_4,
+            R.drawable.prof_ico_5,
+            R.drawable.prof_ico_6 };
+
+  }
 }

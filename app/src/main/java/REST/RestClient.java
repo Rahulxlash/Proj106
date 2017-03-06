6f77c6f791d4c21730f05e33f8fe72a3cd6d0a0b
rahulxlash <rahulsharmpro@gmail.com>
2017-03-06 20:40:00 +0530

    API Retrofit

---
+++ app/src/main/java/REST/RestClient.java
@@ -0,0 +1,32 @@
+package REST;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import REST.Service.AuthService;
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
+
+/**
+ * Created by rahul.sharma01 on 3/6/2017.
+ */
+
+public class RestClient {
+    private static final String BASE_URL = "";
+    private AuthService authService;
+
+    public RestClient() {
+        Gson gson = new GsonBuilder().setDateFormat("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'")
+                .create();
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setEndpoint(BASE_URL)
+                .setConverter(new GsonConverter(gson))
+                .build();
+        authService = restAdapter.create(AuthService.class);
+    }
+
+    public AuthService AuthService() {
+        return authService;
+    }
+}

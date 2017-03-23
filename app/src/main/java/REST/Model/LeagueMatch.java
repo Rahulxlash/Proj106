cf2cfced63af30ece73e605acc7f8917c2d63997
rahulxlash <rahulsharmpro@gmail.com>
2017-03-24 02:23:41 +0530

    Match Card updates

--- app/src/main/java/REST/Model/LeagueMatch.java
+++ app/src/main/java/REST/Model/LeagueMatch.java
@@ -2,6 +2,7 @@ package REST.Model;
 
 import org.parceler.Parcel;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -9,17 +10,36 @@ import java.util.Date;
  */
 @Parcel
 public class LeagueMatch {
-    private int leagueMatchId;
-    private int matchId;
-    private int leagueId;
-    private int teamId1;
-    private int teamId2;
-    private boolean tossDone;
-    private int toss;
-    private int creatorPoint;
-    private int competitorPoint;
-    private Date matchDate;
-    private String venue;
-    private String teamName1;
-    private String teamName2;
+    public int leagueMatchId;
+    public int matchId;
+    public int leagueId;
+    public int teamId1;
+    public int teamId2;
+    public boolean tossDone;
+    public int toss;
+    public int creatorPoint;
+    public int competitorPoint;
+    public Date matchDate;
+    public String venue;
+    public String teamName1;
+    public String teamName2;
+    public int creatorRun;
+    public int creatorWicket;
+    public int competitorRun;
+    public int competitorWicket;
+
+
+    public String getTeams() {
+        return teamName1.trim() + "-" + teamName2.trim();
+    }
+
+    public String getMatchDate() {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        String DateToStr = format.format(matchDate);
+        return DateToStr;
+    }
+
+    public String getMyPoint() {
+        return Integer.toString(creatorPoint - competitorPoint);
+    }
 }
